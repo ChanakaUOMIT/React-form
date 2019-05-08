eee8a51131aee8e943a8499eae12b7424724c85a
ChanakaUOMIT <chanakauomfit@gmail.com>
2019-05-08 11:38:16 +0530

    basic form css part done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,25 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <p>It is Working</p>
+      <form action="">
+      <div className="form-group">
+        <input type="email" placeholder="*Email" />
+        <p className="input-error">This field is required and cannot be empty</p>
+      </div>
+
+      <div className="form-group">
+          <input type="password" placeholder="*password" />
+          <p className="input-error">This field is required and cannot be empty</p>
+        </div>
+
+        <div className="form-group">
+            <input type="password" placeholder="*password confirmation" />
+            <p className="input-error">This field is required and cannot be empty</p>
+          </div>
+
+          <button className="btn" type="submit">Sign Up</button>
+
+    </form>
     </div>
   );
 }
